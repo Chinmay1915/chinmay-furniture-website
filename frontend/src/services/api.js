833eecd941990f0978be4647919ed22d11fa2349
Chinmay <chinmay8287@gmail.com>
2026-03-31 23:56:34 +0530

    Update frontend API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://chinmay-furniture-website.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
@@ -13,3 +13,4 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
+
